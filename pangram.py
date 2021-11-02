ba4b9d6219b40ed5ce76f1f2aba736c82144ba3d
rilwanabimbola7 <44604821+rilwanabimbola7@users.noreply.github.com>
2021-11-02 19:58:59 +0100

    Update pangram.py

--- pangram.py
+++ pangram.py
@@ -1,9 +1,9 @@
-import itertools
+import itertools 
 
-g = list(itertools.product('abcdefghijklmnopqrstuvwxyz', repeat=2))
-alpha = []
+g = list(itertools.product('abcdefghijklmnopqrstuvwxyz', repeat=2))#putting the alphabets in a list and using the itertools.product to perform combination
+alpha = []#creating an empty list to append the output of the for loop
 for k in g:
     p = k[0] + k[1]
     alpha.append(p)
-new_alpha = " ".join([str(a) for a in alpha])
+new_alpha = " ".join([str(a) for a in alpha])#joining the member of the list into a string
 print (new_alpha)
